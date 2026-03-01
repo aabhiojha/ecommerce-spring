e8e3073a4f010ce7c4cf4914f35a3c804d94c2a0
abhishek <aabhiojha08@Outlook.com>
2026-03-01 21:50:59 +0545

    Create user repository with basic query methods

---
+++ src/main/java/dev/abhishek/ecommerce/modules/user/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package dev.abhishek.ecommerce.modules.user.repository;
+
+import dev.abhishek.ecommerce.modules.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+    Optional<User> findByUserName(String userName);
+    Boolean existsByUserName(String userName);
+    Boolean existsByEmail(String email);
+}
